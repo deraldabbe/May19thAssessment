f387ddf1032741118a5188544ed2d46043884124
Derald Abbe <deraldabbe1@gmail.com>
2023-05-20 19:47:13 -0600

    assessment code

---
+++ gitInfo.js
@@ -0,0 +1,93 @@
+/*
+    For this section of the assessment you will be putting together a cheat sheet for common git commands.  
+    You'll provide the command as well as what it does. 
+*/
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Create a variable called 'gitDefinition'.
+    It should be a string containing your best definition of what Git is.
+*/
+
+//CODE HERE
+
+let gitDefinition = `A version control system that allows you to track and work together on projects, a history of the written code, and  also prevents bugs and loss of progress.`
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable called 'gitHubDefinition'.  
+    It should be a string containing your best definition of what GitHub is.
+*/
+
+//CODE HERE
+
+let gitHubDefinition = `A website where you can host git repos and where you can store, share, and collaberate on projects.`
+
+//////////////////PROBLEM 3////////////////////
+/*
+    Create a variable called 'gitInitDefinition'.  
+    It should be a string containing your best definition of what 'git init' does.
+*/
+
+//CODE HERE
+
+let gitInitDefinition = `This is used to initalize a new git repo locally.`
+
+//////////////////PROBLEM 4////////////////////
+/*
+    Create a variable called 'gitCloneDefinition'.  
+    It should be a string containing your best definition of what 'git clone' does.
+*/
+
+//CODE HERE
+
+let gitCloneDefinition = `This is used to create a local copy of a git repo.`
+
+//////////////////PROBLEM 5////////////////////
+/*
+    Create a variable called 'gitStatusDefinition'.  
+    It should be a string containing your best definition of what 'git status' does.
+*/
+
+//CODE HERE
+
+let gitStatusDefinition = `This is used to tell you information about the local git repo.`
+
+//////////////////PROBLEM 6////////////////////
+/*
+    Create a variable called 'gitAddDefinition'.  
+    It should be a string containing your best definition of what 'git add' does.
+
+    Create another variable called 'gitAddCode'.  
+    It should be a string containing the code to add all files.
+*/
+
+//CODE HERE
+
+let gitAddDefinition = `This is used to track and add git repo files.`
+let gitAddCode = `git add .`
+
+//////////////////PROBLEM 7////////////////////
+/*
+    Create a variable called 'gitCommitDefinition'.  
+    It should be a string containing your best definition of what 'git commit' does.
+
+    Create a variable called 'gitCommitCode'.  
+    It should be a string containing the code to commit using the message "initial commit".
+*/
+
+//CODE HERE
+
+let gitCommitDefinition = `This is used to creat a snapshot of the files being tracked.`
+let gitCommitCode = `git commit -m "initial commit`
+
+//////////////////PROBLEM 8////////////////////
+/*
+    Create a variable called 'gitPushDefinition'.  
+    It should be a string containing your best definition of what 'git push' does.
+*/
+
+//CODE HERE
+
+let gitPushDefinition = `This is used to upload local commits to a remote repo like GitHub.`
+
---
+++ sdlc.js
@@ -0,0 +1,27 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Planning : This first phase involves the planning of the projects goals and requirments along with the scope of the project.
+// Analysis of Requirements: This phase has you gather detailed requirments and then analize documnets and priortize expectations.
+// Design: This phase has you turn the requirments in to software "blue prints" and has you address the functionality, performace, security, and scalabilty aspects of the software program.
+// Implementation: This phase is where the code is written and built according the according to the design specs.
+// Testing & Integration: This phase is where the software is tested and preforms correctly and to design specs. This is where bugs are found and fixed.
+// Maintain: This phase is happens after the software is released and has you taking information about protention problems and has to you address them. You will also will be continually be updating and improving the software in this phase.
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const analizeOfRequirments = `This phase has you gather detailed requirments and then analize documnets and priortize expectations.`
+const design = `This phase has you turn the requirments in to software "blue prints" and has you address the functionality, performace, security, and scalabilty aspects of the software program.`
+const implementation = `This phase is where the code is written and built according the according to the design specs.`
+const testingAndIntegration = `This phase is where the software is tested and preforms correctly and to design specs. This is where bugs are found and fixed.`
+const maintain = `This phase is happens after the software is released and has you taking information about protention problems and has to you address them. You will also will be continually be updating and improving the software in this phase.`
+
